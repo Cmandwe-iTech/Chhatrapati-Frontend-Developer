989f0ed514592dafafcdd25de7c708e63c95309d
Chhatrapati Mandwe <mandwec12@gmail.com>
2023-05-27 13:31:41 +0530

    done

--- src/pages/Rocketpage.js
+++ src/pages/Rocketpage.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Modal } from "reactstrap";
 export default function Rocketpage() {
   const [rockets, setRockets] = useState([]);
+  const [prompt, setPrompt] = useState(false);
+  const [details, setDetails] = useState({description:"", type:"", status:""})
   useEffect(() => {
     axios.get("https://spacex-world.onrender.com/rockets").then((res) => {
       setRockets(res.data);
     });
   }, []);
   console.log(rockets);
+  const handleDetails=(x,y,z)=>{
+    setDetails({description:x, type:y, status:z})
+  }
   return (
     <>
       <h1 className="text-white text-center text-xl">Rockets</h1>
@@ -19,15 +25,25 @@ export default function Rocketpage() {
                 src={items.flickr_images[0]}
                 alt="rc1"
                 className="h-80 w-80"
+                onClick={()=>{setPrompt(!prompt);handleDetails(items.description,items.type, items.active)}}
               />
               <p>country: {items.country}</p>
-              <p>description: {items.description}</p>
-              <a href={items.wikipedia} className="my-2 text-lg text-black bg-white">
+              <a
+                href={items.wikipedia}
+                className="my-2 text-lg text-black bg-white"
+              >
                 wikipedia
               </a>
             </div>
           );
         })}
+        <Modal isOpen={prompt} toggle={() => setPrompt(!prompt)}>
+         <div className="h-80 w-80 text-white text-lg flex flex-col mt-2 border-double border-4 border-sky-500 border-white">
+            <p>Descrption: {details.description}</p>
+            <p>Type: {details.type}</p>
+            <p>Current Status: {details.status?"Active":"Currently Not Active"}</p>
+         </div>
+      </Modal>
       </div>
     </>
   );
